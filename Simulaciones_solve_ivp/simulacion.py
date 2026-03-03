5477f250cb48086ee5f5c7523c60593764304edd
María Fernanda Folch <mfernandafolch@uc.cl>
2026-03-03 12:07:12 -0300

    Prueba de modularización de códigos en simulación .py

--- Simulaciones_solve_ivp/simulacion.py
+++ Simulaciones_solve_ivp/simulacion.py
@@ -47,8 +47,20 @@ def simulate_system(excel_path: str, params: list):
     
     return sol
     
+
+paths = [r"C:\Users\MARIA\OneDrive - Universidad Católica de Chile\Escritorio\Concha y Toro\Proyecto_Tintos_CyT_P2\Datos_industriales\CS\51.700 L\Data CS 25 SUC. IVAN VALDES estanque 239.xlsx", 
+         r"C:\Users\MARIA\OneDrive - Universidad Católica de Chile\Escritorio\Concha y Toro\Proyecto_Tintos_CyT_P2\Datos_industriales\CS\100.000 L\Data CS 24 AGROCAUQ estanque 68.xlsx",
+         r"C:\Users\MARIA\OneDrive - Universidad Católica de Chile\Escritorio\Concha y Toro\Proyecto_Tintos_CyT_P2\Datos_industriales\CS\100.000 L\Data CS 24 LOU estanque 54.xlsx"]
+
+params = [5.094427, 5.451988, 2.878541, 2.967765, 429.934043, 279.361563, 123.736979, 278.362199, 0.000100,
+          5.797756, 118.306831, 103.880857, 0.578462, 0.405039]
+
+soluciones = []
+for path in paths:
+    sol = simulate_system(path, params)
+    soluciones.append(sol)
     
-    
+print(soluciones)
     
     
 
